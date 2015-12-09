5eb1d43fb38d6558a0f4e31edc3a7a5a0f40b404
Maks Atygaev <jxcoder@ya.ru>
2015-12-09 09:49:43 +0600

    Implement { gateway.products }

--- src/gateway.products.js
+++ src/gateway.products.js
@@ -1,279 +1,102 @@
-var MongoConfiguration = require('./mongodb.configuration');
-
-var MongoClient = MongoConfiguration.MongoClient;
-
-var ObjectId = MongoConfiguration.ObjectId;
-
-var MongoURL = MongoConfiguration.URL;
-
-var _ = require('./utils');
-
-function GoodsRepository() {};
+var db = require('./db.products');
 
 module.exports = {
   initialize: function(app) {
-    var GoodStructure = {
-      'id': {
-        '$exists': true
-      },
-      'name': {
-        '$exists': true
-      },
-      'cost': {
-        '$exists': true
-      }
-    };
-
-    var GoodProjection = {
-      '_id': 0,
-      'id': 1,
-      'name': 1,
-      'cost': 1
-    };
-
-    function GoodQuery(goodId) {
-      return {
-        'id': goodId,
-        'name': {
-          '$exists': true
-        },
-        'cost': {
-          '$exists': true
+    app.post('/products', function(request, response, next) {
+      var product = {
+        name: request.body.name,
+        cost: {
+          rubles: request.body.cost.rubles,
+          kopeks: request.body.cost.kopeks
         }
       };
-    };
 
-    app.post('/goods', function(request, response) {
-      console.log('[TRACE]', 'GoodsAPI @ { POST /goods }', {request: request.body});
-
-      MongoClient.connect(MongoURL, function(error, db) {
-        console.log('[TRACE]', 'MongoDB.connect');
-
-        if (error) {
-          console.log('[ERROR]', 'MongoDB.connect', 'DB connection problem');
-
-          response.status(500).send({error: 'DB connection problem'});
+      db.save(product).then(function(isOk) {
+        if (isOk) {
+          response.status(201).send({productId: product.id});
         } else {
-          db.collection('goods').count(GoodStructure, function(dbError, dbResponse) {
-            if (dbError) {
-              console.log('[ERROR]', 'Command "count" failed');
-
-              response.status(500).send({error: 'Goods counting problem'});
-            } else {
-              var goodsCount = dbResponse;
-
-              var good = {
-                id: goodsCount + 1,
-                name: request.body.name,
-                cost: {
-                  rubles: request.body.cost.rubles,
-                  kopeks: request.body.cost.kopeks
-                }
-              };
-
-              db.collection('goods').insertOne(good, function(dbError, dbResponse) {
-                try {
-                  console.log('[TRACE]', 'MongoDB.disconnect');
-
-                  db.close();
-                } catch(e) {
-                  console.log('[ERROR}', 'MongoDB.disconnect', 'DB disconnection problem');
-                }
-
-                if (dbError) {
-                  console.log('[ERROR]', 'Command "insertOne" failed');
-
-                  response.status(500).send({error: 'Good saving problem'});
-                } else {
-                  response.status(201).send({goodId: good.id});
-                }
-              });
-            }
-          });
+          response.send(500);
         }
+      })
+      .catch(function(e) {
+        next(e);
       });
     });
 
-    app.get('/goods', function(request, response) {
-      console.log('[TRACE]', 'GoodsAPI @ { GET /goods }', {request: request.params});
-
-      MongoClient.connect(MongoURL, function(error, db) {
-        console.log('[TRACE]', 'MongoDB.connect');
-
-        if (error) {
-          console.log('[ERROR]', 'MongoDB.connect', 'DB connection problem');
-
-          response.status(500).send({error: 'DB connection problem'});
-        } else {
-          db.collection('goods').find(GoodStructure, GoodProjection, function(dbError, dbResponse) {
-            if (dbError) {
-              console.log('[ERROR]', 'Command "find" failed');
-
-              response.status(500).send({error: 'Goods loading problem'});
-            } else {
-              dbResponse.toArray(function(dbError, documents) {
-                try {
-                  console.log('[TRACE]', 'MongoDB.disconnect');
-
-                  db.close();
-                } catch(e) {
-                  console.log('ERROR]', 'MongoDB.disconnect', 'DB disconnection problem');
-                }
-
-                response.status(200).send({good: documents});
-              });
-            }
-          });
-        }
+    app.get('/products', function(request, response, next) {
+      db.find().then(function(products) {
+        response.status(200).send({products: products});
+      })
+      .catch(function(e) {
+        next(e);
       });
     });
 
-    app.get('/goods/:goodId', function(request, response) {
-      console.log('[TRACE]', 'GoodsAPI @ { GET /goods/:goodId }', {request: request.params});
+    app.get('/products/:productId', function(request, response, next) {
+      var productId = parseInt(request.params.productId);
 
-      if (!_.isNumber(request.params.goodId)) {
-        response.status(400).send({error: 'Invalid good identifier'});
-      } else {
-        var goodId = parseInt(request.params.goodId, 10);
-
-        console.log('[TRACE]', 'GoodsAPI @ { GET /goods/' + goodId + ' }');
-
-        MongoClient.connect(MongoURL, function(error, db) {
-          console.log('[TRACE]', 'MongoDB.connect');
-
-          if (error) {
-            console.log('[ERROR]', 'MongoDB.connect', 'DB connection problem');
-
-            response.status(500).send({error: 'DB connection problem'});
-          } else {
-            db.collection('goods').findOne(GoodStructure, GoodProjection, function(dbError, document) {
-              if (dbError) {
-                console.log('[ERROR]', 'Command "findOne" failed');
-
-                response.status(500).send({error: 'Good loading problem'});
-              } else {
-                try {
-                  console.log('[TRACE]', 'MongoDB.disconnect');
-
-                  db.close();
-                } catch(e) {
-                  console.log('[ERROR]', 'MongoDB.disconnect', 'DB disconnection problem');
-                }
-
-                if (document == null) {
-                  response.status(404).send({error: 'Good not found'});
-                } else {
-                  response.status(200).send({good: document});
-                }
-              }
-            });
-          }
-        });
-      }
+      db.find(productId).then(function(product) {
+        if (product != null) {
+          response.status(200).send({product: product});
+        } else {
+          response.status(400);
+        }
+      })
+      .catch(function(e) {
+        next(e);
+      });
     });
 
-    app.post('/goods/:goodId', function(request, response) {
-      console.log('[TRACE]', 'GoodsAPI @ { POST /goods/:goodId }', {request: request.body});
+    app.put('/products/:productId', function(request, response, next) {
+      var productId = parseInt(request.params.productId);
 
-      if (!_.isNumber(request.params.goodId)) {
-        response.status(400).send({error: 'Invalid good identifier'});
-      } else {
-        var goodId = parseInt(request.params.goodId, 10);
+      var product = {
+        id: productId,
+        name: request.body.name,
+        cost: {
+          rubles: request.body.cost.rubles,
+          kopeks: request.body.cost.kopeks
+        }
+      };
 
-        console.log('[TRACE]', 'GoodsAPI @ { POST /goods/' + goodId + ' }');
-
-        MongoClient.connect(MongoURL, function(error, db) {
-          console.log('[TRACE]', 'MongoDB.connect');
-
-          if (error) {
-            console.log('[ERROR]', 'MongoDB.connect', 'DB connection problem');
-
-            response.status(500).send({error: 'DB connection problem'});
-          } else {
-            var dbCommand = {
-              '$set': {}
-            };
-
-            if (request.body.name) {
-              dbCommand['$set']['name'] = request.body.name;
-            }
-
-            if (request.body.cost && request.body.cost.rubles) {
-              dbCommand['$set']['cost.rubles'] = request.body.cost.rubles;
-            }
-
-            if (request.body.cost && request.body.cost.kopeks) {
-              dbCommand['$set']['cost.kopeks'] = request.body.cost.kopeks;
-            }
-
-            db.collection('goods').updateOne(GoodQuery(goodId), dbCommand, function(dbError, dbResponse) {
-              if (dbError) {
-                console.log('[ERROR]', 'Command "update" failed');
-
-                response.status(500).send({error: 'Good updating problem'});
-              } else {
-                try {
-                  console.log('[TRACE]', 'MongoDB.disconnect');
-
-                  db.close();
-                } catch(e) {
-                  console.log('[ERROR]', 'MongoDB.disconnect', 'DB disconnection problem');
-                }
-
-                if (dbResponse.matchedCount == 0) {
-                  response.status(404).send({error: 'Good not found'});
-                } else {
-                  response.status(204).end();
-                }
-              }
-            });
-          }
-        });
-      }
+      db.update(product).then(function(isOk) {
+        if (isOk) {
+          response.status(204);
+        } else {
+          response.status(500);
+        }
+      })
+      .catch(function(e) {
+        next(e);
+      });
     });
 
-    app.delete('/goods/:goodId', function(request, response) {
-      console.log('[TRACE]', 'GoodsAPI @ { DELETE /goods/:goodId }', {request: request.body});
+    app.delete('/products/:productId', function(request, response, next) {
+      var productId = parseInt(request.params.productId);
 
-      if (!_.isNumber(request.params.goodId)) {
-        response.status(400).send({error: 'Invalid good identifier'});
-      } else {
-        var goodId = parseInt(request.params.goodId, 10);
+      db.remove(productId).then(function(isOk) {
+        if (isOk) {
+          response.status(204);
+        } else {
+          response.status(500);
+        }
+      })
+      .catch(function(e) {
+        next(e);
+      });
+    });
 
-        console.log('[TRACE]', 'GoodsAPI @ { DELETE /goods/' + goodId + ' }');
-
-        MongoClient.connect(MongoURL, function(error, db) {
-          console.log('[TRACE]', 'MongoDB.connect');
-
-          if (error) {
-            console.log('[ERROR]', 'MongoDB.connect', 'DB connection problem');
-
-            response.status(500).send({error: 'DB connection problem'});
-          } else {
-            db.collection('goods').removeOne(GoodQuery(goodId), function(dbError, dbResponse) {
-              if (dbError) {
-                console.log('[ERROR]', 'Command "remove" failed');
-
-                response.status(500).send({error: 'Good removing problem'});
-              } else {
-                try {
-                  console.log('[TRACE]', 'MongoDB.disconnect');
-
-                  db.close();
-                } catch(e) {
-                  console.log('[ERROR]', 'MongoDB.disconnect', 'DB disconnection problem');
-                }
-
-                if (dbResponse.deletedCount == 0) {
-                  response.status(404).send({error: 'Good not found'});
-                } else {
-                  response.status(204).end();
-                }
-              }
-            });
-          }
-        });
-      }
+    app.delete('/products', function(request, response, next) {
+      db.remove().then(function(isOk) {
+        if (isOk) {
+          response.send(204);
+        } else {
+          response.send(500);
+        }
+      })
+      .catch(function(e) {
+        next(e);
+      });
     });
   }
 };
