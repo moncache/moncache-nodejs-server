c45767c43f8f597a32a60c8c9006783c4ef3d011
Maks Atygaev <jxcoder@ya.ru>
2015-12-09 09:42:28 +0600

    Implement { gateway.offices }

--- src/gateway.offices.js
+++ src/gateway.offices.js
@@ -1,288 +1,98 @@
-var MongoConfiguration = require('./mongodb.configuration');
-
-var MongoClient = MongoConfiguration.MongoClient;
-
-var ObjectId = MongoConfiguration.ObjectId;
-
-var MongoURL = MongoConfiguration.URL;
-
-var _ = require('./utils');
+var db = require('./db.offices');
 
 module.exports = {
   initialize: function(app) {
-    var OfficeStructure = {
-      'id': {
-        '$exists': true
-      },
-      'name': {
-        '$exists': true
-      },
-      'address': {
-        '$exists': true
-      }
-    };
-
-    var OfficeProjection = {
-      '_id': 0,
-      'id': 1,
-      'name': 1,
-      'address': 1
-    };
-
-    function OfficeQuery(officeId) {
-      return {
-        'id': officeId,
-        'name': {
-          '$exists': true
-        },
-        'address': {
-          '$exists': true
-        }
+    app.post('/offices', function(request, response, next) {
+      var office = {
+        name: request.body.name,
+        address: request.body.address
       };
-    };
 
-    app.delete('/offices', function(request, response) {
-      MongoClient.connect(MongoURL, function(error, db) {
-        console.log('[?]', error);
+      console.log(request.body);
 
-        db.collection('offices').deleteMany({}, function(error, result) {
-          console.log('[!]', error, result);
+      db.save(office).then(function(isOk) {
+        if (isOk) {
+          response.status(201).send({officeId: office.id});
+        } else {
+          response.send(500);
+        }
+      })
+      .catch(function(e) {
+        next(e);
+      });
+    });
 
-          db.close();
+    app.get('/offices', function(request, response, next) {
+      db.find().then(function(offices) {
+        response.status(200).send({offices: offices});
+      })
+      .catch(function(e) {
+        next(e);
+      });
+    });
 
+    app.get('/offices/:officeId', function(request, response, next) {
+      var officeId = parseInt(request.params.officeId);
+
+      db.find(officeId).then(function(office) {
+        if (office != null) {
+          response.status(200).send({office: office});
+        } else {
+          response.status(400);
+        }
+      })
+      .catch(function(e) {
+        next(e);
+      });
+    });
+
+    app.put('/offices/:officeId', function(request, response, next) {
+      var officeId = parseInt(request.params.officeId);
+
+      var office = {
+        id: officeId,
+        name: request.body.name,
+        address: request.body.address
+      };
+
+      db.update(office).then(function(isOk) {
+        if (isOk) {
+          response.status(204);
+        } else {
+          response.status(500);
+        }
+      })
+      .catch(function(e) {
+        next(e);
+      });
+    });
+
+    app.delete('/offices/:officeId', function(request, response, next) {
+      var officeId = parseInt(request.params.officeId);
+
+      db.remove(officeId).then(function(isOk) {
+        if (isOk) {
+          response.status(204);
+        } else {
+          response.status(500);
+        }
+      })
+      .catch(function(e) {
+        next(e);
+      });
+    });
+
+    app.delete('/offices', function(request, response, next) {
+      db.remove().then(function(isOk) {
+        if (isOk) {
           response.send(204);
-        });
-      });
-    });
-
-    app.post('/offices', function(request, response) {
-      console.log('[TRACE]', 'OfficesAPI @ { POST /offices }', {request: request.body});
-
-      MongoClient.connect(MongoURL, function(error, db) {
-        console.log('[TRACE]', 'MongoDB.connect');
-
-        if (error) {
-          console.log('[ERROR]', 'MongoDB.connect', 'DB connection problem');
-
-          response.status(500).send({error: 'DB connection problem'});
         } else {
-          db.collection('offices').count(OfficeStructure, function(dbError, dbResponse) {
-            if (dbError) {
-              console.log('[ERROR]', 'Command "count" failed');
-
-              response.status(500).send({error: 'Offices counting problem'});
-            } else {
-              var officesCount = dbResponse;
-
-              var office = {
-                id: officesCount + 1,
-                name: request.body.name,
-                address: request.body.address
-              };
-
-              db.collection('offices').insertOne(office, function(dbError, dbResponse) {
-                try {
-                  console.log('[TRACE]', 'MongoDB.disconnect');
-
-                  db.close();
-                } catch(e) {
-                  console.log('[ERROR}', 'MongoDB.disconnect', 'DB disconnection problem');
-                }
-
-                if (dbError) {
-                  console.log('[ERROR]', 'Command "insertOne" failed');
-
-                  response.status(500).send({error: 'Office saving problem'});
-                } else {
-                  if (dbResponse.insertedCount == 1) {
-                    response.status(201).send({officeId: office.id});
-                  } else {
-                    response.status(500).send({error: 'Office saving problem'});
-                  }
-                }
-              });
-            }
-          });
+          response.send(500);
         }
+      })
+      .catch(function(e) {
+        next(e);
       });
-    });
-
-    app.get('/offices', function(request, response) {
-      console.log('[TRACE]', 'OfficesAPI @ { GET /offices }', {request: request.params});
-
-      MongoClient.connect(MongoURL, function(error, db) {
-        console.log('[TRACE]', 'MongoDB.connect');
-
-        if (error) {
-          console.log('[ERROR]', 'MongoDB.connect', 'DB connection problem');
-
-          response.status(500).send({error: 'DB connection problem'});
-        } else {
-          db.collection('offices').find(OfficeStructure, OfficeProjection, function(dbError, dbResponse) {
-            if (dbError) {
-              console.log('[ERROR]', 'Command "find" failed');
-
-              response.status(500).send({error: 'Offices loading problem'});
-            } else {
-              dbResponse.toArray(function(dbError, documents) {
-                try {
-                  console.log('[TRACE]', 'MongoDB.disconnect');
-
-                  db.close();
-                } catch(e) {
-                  console.log('ERROR]', 'MongoDB.disconnect', 'DB disconnection problem');
-                }
-
-                response.status(200).send({offices: documents});
-              });
-            }
-          });
-        }
-      });
-    });
-
-    app.get('/offices/:officeId', function(request, response) {
-      console.log('[TRACE]', 'OfficesAPI @ { GET /offices/:officeId }', {request: request.params});
-
-      if (!_.isNumber(request.params.officeId)) {
-        response.status(400).send({error: 'Invalid office identifier'});
-      } else {
-        var officeId = parseInt(request.params.officeId, 10);
-
-        console.log('[TRACE]', 'OfficesAPI @ { GET /offices/' + officeId + ' }');
-
-        MongoClient.connect(MongoURL, function(error, db) {
-          console.log('[TRACE]', 'MongoDB.connect');
-
-          if (error) {
-            console.log('[ERROR]', 'MongoDB.connect', 'DB connection problem');
-
-            response.status(500).send({error: 'DB connection problem'});
-          } else {
-            db.collection('offices').findOne(OfficeQuery(officeId), OfficeProjection, function(dbError, document) {
-              if (dbError) {
-                console.log('[ERROR]', 'Command "findOne" failed');
-
-                response.status(500).send({error: 'Office loading problem'});
-              } else {
-                try {
-                  console.log('[TRACE]', 'MongoDB.disconnect');
-
-                  db.close();
-                } catch(e) {
-                  console.log('[ERROR]', 'MongoDB.disconnect', 'DB disconnection problem');
-                }
-
-                if (document == null) {
-                  response.status(404).send({error: 'Office not found'});
-                } else {
-                  response.status(200).send({office: document});
-                }
-              }
-            });
-          }
-        });
-      }
-    });
-
-    app.post('/offices/:officeId', function(request, response) {
-      console.log('[TRACE]', 'OfficesAPI @ { POST /offices/:officeId }', {request: request.body});
-
-      if (!_.isNumber(request.params.officeId)) {
-        response.status(400).send({error: 'Invalid office identifier'});
-      } else {
-        var officeId = parseInt(request.params.officeId, 10);
-
-        console.log('[TRACE]', 'OfficesAPI @ { POST /offices/' + officeId + ' }');
-
-        MongoClient.connect(MongoURL, function(error, db) {
-          console.log('[TRACE]', 'MongoDB.connect');
-
-          if (error) {
-            console.log('[ERROR]', 'MongoDB.connect', 'DB connection problem');
-
-            response.status(500).send({error: 'DB connection problem'});
-          } else {
-            var dbCommand = {
-              '$set': {}
-            };
-
-            if (request.body.name) {
-              dbCommand['$set']['name'] = request.body.name;
-            }
-
-            if (request.body.address) {
-              dbCommand['$set']['address'] = request.body.address;
-            }
-
-            db.collection('offices').updateOne(OfficeQuery(officeId), dbCommand, function(dbError, dbResponse) {
-              if (dbError) {
-                console.log('[ERROR]', 'Command "update" failed');
-
-                response.status(500).send({error: 'Office updating problem'});
-              } else {
-                try {
-                  console.log('[TRACE]', 'MongoDB.disconnect');
-
-                  db.close();
-                } catch(e) {
-                  console.log('[ERROR]', 'MongoDB.disconnect', 'DB disconnection problem');
-                }
-
-                if (dbResponse.matchedCount == 0) {
-                  response.status(404).send({error: 'Office not found'});
-                } else {
-                  response.status(204).end();
-                }
-              }
-            });
-          }
-        });
-      }
-    });
-
-    app.delete('/offices/:officeId', function(request, response) {
-      console.log('[TRACE]', 'OfficesAPI @ { DELETE /offices/:officeId }', {request: request.body});
-
-      if (!_.isNumber(request.params.officeId)) {
-        response.status(400).send({error: 'Invalid office identifier'});
-      } else {
-        var officeId = parseInt(request.params.officeId, 10);
-
-        console.log('[TRACE]', 'OfficesAPI @ { DELETE /offices/' + officeId + ' }');
-
-        MongoClient.connect(MongoURL, function(error, db) {
-          console.log('[TRACE]', 'MongoDB.connect');
-
-          if (error) {
-            console.log('[ERROR]', 'MongoDB.connect', 'DB connection problem');
-
-            response.status(500).send({error: 'DB connection problem'});
-          } else {
-            db.collection('offices').removeOne(OfficeQuery(officeId), function(dbError, dbResponse) {
-              if (dbError) {
-                console.log('[ERROR]', 'Command "remove" failed');
-
-                response.status(500).send({error: 'Office removing problem'});
-              } else {
-                try {
-                  console.log('[TRACE]', 'MongoDB.disconnect');
-
-                  db.close();
-                } catch(e) {
-                  console.log('[ERROR]', 'MongoDB.disconnect', 'DB disconnection problem');
-                }
-
-                if (dbResponse.deletedCount == 0) {
-                  response.status(404).send({error: 'Office not found'});
-                } else {
-                  response.status(204).end();
-                }
-              }
-            });
-          }
-        });
-      }
     });
   }
 };
